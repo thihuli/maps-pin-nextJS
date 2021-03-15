b435bfa3e66d77ccee8f5d0a4dc9d82f8ba4fef2
thihuli <thiagocandidof@hotmail.com>
2021-03-15 02:12:39 -0300

    create and config boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,20 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
 
-export default function Home() {
+type Props = {
+  title: string;
+};
+
+export default function Home({ title = 'React Avançado' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -49,7 +51,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -61,5 +62,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
